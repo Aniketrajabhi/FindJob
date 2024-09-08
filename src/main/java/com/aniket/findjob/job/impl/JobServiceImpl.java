7d484a3e289a17dcb8d4094249ea8ac719ac943a
Aniketrajabhi <aniketrajabhi123@gmail.com>
2024-09-08 08:25:16 +0530

    @Request Mapping and Updation(PUT)

--- src/main/java/com/aniket/findjob/job/impl/JobServiceImpl.java
+++ src/main/java/com/aniket/findjob/job/impl/JobServiceImpl.java
@@ -3,6 +3,7 @@ import com.aniket.findjob.job.Job;
 import com.aniket.findjob.job.JobService;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -23,11 +24,39 @@ public class JobServiceImpl implements JobService {
 
     @Override
     public Job getJobById(Long id) {
-        for(Job job: jobs){
-            if(job.getId().equals(id)){
+        for (Job job : jobs) {
+            if (job.getId().equals(id)) {
                 return job;
             }
         }
         return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> it = jobs.iterator();
+        while (it.hasNext()) {
+            Job job = it.next();
+            if (job.getId().equals(id)) {
+                it.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJobById(Long id, Job updatedJob) {
+        for (Job job : jobs) {
+            if (job.getId().equals(id)) {
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription(updatedJob.getDescription());
+                job.setMaxSalary(updatedJob.getMaxSalary());
+                job.setMinSalary(updatedJob.getMinSalary());
+                job.setLocation(updatedJob.getLocation());
+                return true;
+            }
+        }
+        return false;
     }
 }
